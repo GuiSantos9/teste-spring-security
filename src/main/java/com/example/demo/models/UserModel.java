9df19f15de1502021563d8ef0bd3c0c4b0b29880
Guilherme Augusto dos Santos <guilhermeaugusto923@gmail.com>
2026-04-27 22:14:51 -0300

    commits

--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -4,17 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String user;
     private String login;
     private String passowrd;
+    private UserRole role;
 
     public UserModel(){}
 
@@ -26,19 +33,11 @@ public class UserModel {
         this.id = id;
     }
 
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
     public String getLogin() {
         return login;
     }
 
-    public void setLogin(String login) {
+    public void setLogin(String user) {
         this.login = login;
     }
 
@@ -48,5 +47,31 @@ public class UserModel {
 
     public void setPassowrd(String passowrd) {
         this.passowrd = passowrd;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    //configurando as permissões do administrador
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN)  return List.of
+                (new SimpleGrantedAuthority("ROLE_ADMIN"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_EXECUTIVO"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
     }
 }
